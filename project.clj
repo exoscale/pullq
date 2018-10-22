dd94aaa3e745a1fb50595eab7b8210d7a7e3afd3
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-10-22 10:35:37 +0200

    project: re-frame 10x is unnecessary here

--- project.clj
+++ project.clj
@@ -18,7 +18,6 @@
   :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
   :profiles
   {:dev  {:dependencies [[binaryage/devtools "0.9.10"]
-                         [day8.re-frame/re-frame-10x "0.3.3"]
                          [day8.re-frame/tracing "0.5.1"]
                          [figwheel-sidecar "0.5.16"]
                          [cider/piggieback "0.3.5"]]
@@ -34,11 +33,6 @@
                     :output-dir           "resources/public/js/compiled/out"
                     :asset-path           "js/compiled/out"
                     :source-map-timestamp true
-                    :preloads             [devtools.preload
-                                           day8.re-frame-10x.preload]
-                    :closure-defines
-                    {"re_frame.trace.trace_enabled_QMARK_"        true
-                     "day8.re_frame.tracing.trace_enabled_QMARK_" true}
                     :external-config
                     {:devtools/config {:features-to-install :all}}}}
     {:id           "min"
